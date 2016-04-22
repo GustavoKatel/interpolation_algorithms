e27990cb1474e6b9a09f0393819d2f09a1a38c75
Gustavo Sampaio <gbritosampaio@gmail.com>
2016-04-21 21:33:42 -0300

    Rust: Random test, Error estimation, Better presentation

--- rust/lagrange/src/lib.rs
+++ rust/lagrange/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod lagrange {
 
-    pub fn interpolation(points: &[(f32, f32)], x : f32) -> f32 {
+    pub fn interpolation(points: &[(f64, f64)], x : f64) -> f64 {
         let mut i : i32 = -1;
         let res = points
                     .iter()
-                    .fold(0.0 as f32, |acc, &(xi, yi)| {
+                    .fold(0.0 as f64, |acc, &(xi, yi)| {
                         i+=1;
                         return acc + yi * lsum(points, i, x);
                     });
@@ -14,20 +14,20 @@ pub mod lagrange {
         return res;
     }
 
-    fn lsum(points: &[(f32, f32)], i: i32, x: f32) -> f32 {
+    fn lsum(points: &[(f64, f64)], i: i32, x: f64) -> f64 {
         let (xi, _) = points[i as usize];  // usize overflow?
 
-        let mut upper : Vec<f32> = points
+        let mut upper : Vec<f64> = points
                             .iter()
                             .map( |&(xj, yj)| x - xj ).collect();
         upper[i as usize] = 1.0;
-        let upper_res : f32 = upper.into_iter().fold(1.0 as f32, |acc, v| acc * v );
+        let upper_res : f64 = upper.into_iter().fold(1.0 as f64, |acc, v| acc * v );
 
-        let mut lower : Vec<f32> = points
+        let mut lower : Vec<f64> = points
                             .iter()
                             .map( |&(xj, yj)| xi - xj ).collect();
         lower[i as usize] = 1.0;
-        let lower_res : f32 = lower.into_iter().fold(1.0 as f32, |acc, v| acc * v );
+        let lower_res : f64 = lower.into_iter().fold(1.0 as f64, |acc, v| acc * v );
 
         return upper_res / lower_res;
     }
--- rust/main/src/main.rs
+++ rust/main/src/main.rs
@@ -1,13 +1,42 @@
 extern crate lagrange;
+extern crate rand;
+
+use rand::distributions::{IndependentSample, Range};
+
+fn func1(x: f64) -> f64 {
+    return (x.powi(4) / 11.0).sqrt() - 2.0 * x.powi(3) + x.powi(2) - 37.0;
+}
 
 fn main() {
 
-    let points : [(f32, f32); 3] = [(0.0, 0.0), (1.0, 1.0), (2.0, 4.0)];
+    let points : [(f64, f64); 11] = [
+        (0.0, -37.0),
+        (1.0, -37.6984886554),
+        (2.0, -47.7939546217),
+        (3.0, -79.2863978988),
+        (4.0, -144.175818487),
+        (5.0, -254.462216386),
+        (6.0, -422.145591595),
+        (7.0, -659.225944116),
+        (8.0, -977.703273947),
+        (9.0, -1389.57758109),
+        (10.0, -1906.84886554)
+    ];
 
-    let x : f32 = 1.5;
+    let (x0, _) : (f64, _) = points[0];
+    let (xn, _) : (f64, _) = points[points.len()-1];
 
-    let y = lagrange::lagrange::interpolation(&points, x);
+    let between = Range::new(x0, xn);
+    let mut rng = rand::thread_rng();
 
-    println!("{:?}", y);
+    let x : f64 = between.ind_sample(&mut rng);
+
+    let px = lagrange::lagrange::interpolation(&points, x);
+
+    let fx = func1(x);
+
+    let epsilon = (fx - px).abs();
+
+    println!("x = {:?}, P(x) = {:?}, f(x) = {:?}, ξ = {:?}", x, px, fx, epsilon);
 
 }
